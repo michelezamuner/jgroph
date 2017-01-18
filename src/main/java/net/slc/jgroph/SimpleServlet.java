e48c8a4e45f48cf3b94d561aedcc367314fb5a8b
Michele Zamuner <michelezamuner@gmail.com>
2017-01-18 11:27:37 +0100

    Using annotations

--- src/main/java/net/slc/jgroph/SimpleServlet.java
+++ src/main/java/net/slc/jgroph/SimpleServlet.java
@@ -1,9 +1,14 @@
 package net.slc.jgroph;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet(name="jgroph", urlPatterns={"/"})
 public class SimpleServlet extends HttpServlet
 {
     @Override
