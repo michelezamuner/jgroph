ece3052936cd1cc1c305aa6305e05eae217af088
Michele Zamuner <michelezamuner@gmail.com>
2017-01-19 17:58:02 +0100

    Removed annotations

--- src/main/java/net/slc/jgroph/SimpleServlet.java
+++ src/main/java/net/slc/jgroph/SimpleServlet.java
@@ -6,9 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name="jgroph", urlPatterns={"/"})
 public class SimpleServlet extends HttpServlet
 {
     @Override
